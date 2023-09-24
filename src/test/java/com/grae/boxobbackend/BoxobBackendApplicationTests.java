f20cb7f7ca3ae4f919d1858292575fb026ddfe3a
exdeeface <bowmang@lsbu.ac.uk>
2023-09-24 22:09:06 +0100

    added first unit test and jacoco integration

--- src/test/java/com/grae/boxobbackend/BoxobBackendApplicationTests.java
+++ src/test/java/com/grae/boxobbackend/BoxobBackendApplicationTests.java
@@ -12,6 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.junit.runner.RunWith;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -32,26 +33,20 @@ class BoxobBackendApplicationTests {
 	FilmController filmController;
 
 	@Test
-	void testGetFilm() throws Exception {
-		FilmEntity film1 = new FilmEntity(1, "ACADEMY DINOSAUR",
-				"A Epic Drama of a Feminist And a Mad Scientist who must Battle a Teacher in The Canadian Rockies",
-				86, 2006,"PG", 1, 6, 0.99);
+	void testGetFilms() throws Exception {
+		FilmEntity film0 = new FilmEntity(1, "ACADEMY DINOSAUR", "A Epic Drama of a Feminist And a Mad Scientist who must Battle a Teacher in The Canadian Rockies", 86, 2006,"PG", 1, 6, 0.99);
+		FilmEntity film1 = new FilmEntity(500, "KISS GLORY", "A Lacklusture Reflection of a Girl And a Husband who must Find a Robot in The Canadian Rockies", 163, 2006,"PG-13", 1, 5, 4.99);
+		FilmEntity film2 = new FilmEntity(1000, "ZORRO ARK", "A Intrepid Panorama of a Mad Scientist And a Boy who must Redeem a Boy in A Monastery", 50, 2008,"NC-17", 1, 3, 4.99);
 
-
-		FilmEntity film2 = new FilmEntity(500, "KISS GLORY",
-				"A Lacklusture Reflection of a Girl And a Husband who must Find a Robot in The Canadian Rockies",
-				163, 2006,"PG-13", 1, 5, 4.99);
-
-		FilmEntity film3 = new FilmEntity(1000, "ZORRO ARK",
-				"A Intrepid Panorama of a Mad Scientist And a Boy who must Redeem a Boy in A Monastery",
-				50, 2008,"NC-17", 1, 3, 4.99);
-
-		List<FilmEntity> films = Arrays.asList(film1, film2, film3);
+		List<FilmEntity> films = Arrays.asList(film0, film1, film2);
 		when(filmController.getFilms()).thenReturn(films);
 
 		mvc.perform(get("/films")
 			.accept(MediaType.APPLICATION_JSON))
-			.andExpect(jsonPath("$[0].film_id").value(films.get(0).getFilmId()));
+			.andExpect(jsonPath("$[0].film_id").value(film0.getFilmId()))
+			.andExpect(jsonPath("$[1].film_id").value(film1.getFilmId()))
+			.andExpect(jsonPath("$[2].film_id").value(film2.getFilmId()))
+			.andExpect(jsonPath("$", hasSize(3)));
 	}
 
 }
