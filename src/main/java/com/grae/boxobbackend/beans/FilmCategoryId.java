097e1ea5d1fbe52349d871e06b112f005c203f26
exdeeface <bowmang@lsbu.ac.uk>
2023-09-28 22:50:33 +0100

    removed redundant code

--- src/main/java/com/grae/boxobbackend/beans/FilmCategoryId.java
+++ src/main/java/com/grae/boxobbackend/beans/FilmCategoryId.java
@@ -3,7 +3,6 @@ package com.grae.boxobbackend.beans;
 import jakarta.persistence.Embeddable;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 @Embeddable
 public class FilmCategoryId implements Serializable {
@@ -15,22 +14,6 @@ public class FilmCategoryId implements Serializable {
         this.category_id = category_id;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        FilmCategoryId that = (FilmCategoryId) o;
-        return Objects.equals(film_id, that.film_id) && Objects.equals(category_id, that.category_id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(film_id, category_id);
-    }
-
     public Integer getFilm_id() { return film_id; }
     public Integer getCategory_id() { return category_id; }
-
-    public void setFilm_id(Integer film_id) { this.film_id = film_id; }
-    public void setCategory_id(Integer category_id) { this.category_id = category_id; }
 }
--- src/main/java/com/grae/boxobbackend/entity/ActorEntity.java
+++ src/main/java/com/grae/boxobbackend/entity/ActorEntity.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.sql.Date;
-import java.time.LocalDate;
 import java.util.List;
 
 @Getter
@@ -26,23 +25,4 @@ public class ActorEntity {
 
     @ManyToMany(mappedBy = "actors", fetch = FetchType.LAZY)
     private List<FilmEntity> films;
-
-    public ActorEntity(Integer actor_id, String first_name, String last_name) {
-        this.actor_id = actor_id;
-        this.first_name = first_name;
-        this.last_name = last_name;
-        setLast_update();
-    }
-
-    public Integer getActor_id() { return actor_id; }
-    public String getFirst_name() { return first_name;}
-    public String getLast_name() { return last_name; }
-    public Date getLast_update() { return last_update; }
-    public List<FilmEntity> getFilms() { return films; }
-
-    public void setActor_id(Integer actor_id) { this.actor_id = actor_id; }
-    public void setFirst_name(String first_name) { this.first_name = first_name;}
-    public void setLast_name(String last_name) { this.last_name = last_name; }
-    public void setLast_update() { this.last_update = Date.valueOf(LocalDate.now()); }
-    public void setFilms(List<FilmEntity> films) { this.films = films; }
 }
